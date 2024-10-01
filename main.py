1b94cc140bc2a98ed2d994428f7b2d8dd855f3bf
Rostislav2007 <kukolevrostislav@gmail.com>
2024-10-01 21:14:14 +0300

    комментарий добавление

---
+++ main.py
@@ -0,0 +1,20 @@
+# class convertTemp:
+#     count = 0
+#     @staticmethod
+#     def f_to_c(temp):
+#         convertTemp.count += 1
+#         return (temp - 32) / 1.8
+#     @staticmethod
+#     def c_to_f(temp):
+#         convertTemp.count += 1
+#         return (temp * 1.8) + 32
+#     @staticmethod
+#     def get_count_op():
+#         return convertTemp.count
+    
+# convert = convertTemp()
+# print(f"F to C: {convert.f_to_c(140)}")
+# print(f"F to C: {convert.f_to_c(340)}")
+# print(f"C to F: {convert.c_to_f(25)}")
+# print(f"C to F: {convert.c_to_f(-12)}")
+# print(f"count operation {convert.get_count_op()}")
